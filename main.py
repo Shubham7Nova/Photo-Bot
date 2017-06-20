1f01245fd4640ff5a32053499f6b0a23cd5241b1
Shubham Kukreti <skukreti.kukreti5@gmail.com>
2017-06-20 16:52:08 +0530

    Function added for getting our own posts.

--- main.py
+++ main.py
@@ -59,3 +59,20 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+#Function for getting our own posts.
+
+def get_own_post():
+    request_url = (BASE_URL + "users/self/media/recent/?access_token=%s") % (ACCESS_TOKEN)
+    print ("GET request URL: %s") % (request_url)
+    own_media = requests.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if (own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media ['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url,image_name)
+            print "Your image has been downloaded."
+        else:
+            print "Post does not exist!"
+    else:
+        print "Status code other than 200 received."
