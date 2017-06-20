d6ce290957c13c02c73288e94ea34715bb270e15
Shubham Kukreti <skukreti.kukreti5@gmail.com>
2017-06-20 16:40:12 +0530

    Added function for getting recent information about a user using User ID.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from keys import ACCESS_TOKEN
 import requests
+import urllib
 BASE_URL = "https://api.instagram.com/v1/"
 
-#Defining function for our own info
+#Defining function for prinitng our own info.
 
 def self_info():
     request_url = (BASE_URL + "users/self/?access_token=%s") % (ACCESS_TOKEN)
@@ -35,6 +36,26 @@ def get_user_id(insta_username):
     else:
         print "Status code other than 200 received"
     exit()
-print get_user_id()
+print get_user_id("payal.mittal95")
 
+#Function for getting recent information about a user using User ID.
+
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print "Invalid Username."
+        exit()
+    request_url = (BASE_URL + "users/%s/?access_token=%s") % (user_id,ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
 
