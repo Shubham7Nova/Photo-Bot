397b2e102eda654cf71c6c47d5ffa193010ab6c7
Shubham Kukreti <skukreti.kukreti5@gmail.com>
2017-06-25 22:46:21 +0530

    Added functions for getting images with max/min likes..Added function for searching for an activity/Event by subtrends(Hashtags).

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ from textblob.sentiments import NaiveBayesAnalyzer
 
 BASE_URL = "https://api.instagram.com/v1/"
 
+#Defining likes_count for storing id of posts with minimum or maximum
+likes_count = {}
+
 #Defining function for prinitng our own info.
 
 def self_info():
     request_url = (BASE_URL + "users/self/?access_token=%s") % (ACCESS_TOKEN)
     print "GET request URL for self info : %s." % (request_url)
+    #Syntax for using GET and JSON Parser(Will be the same for every GET request).
     user_info = requests.get(request_url).json()
     print user_info
     if user_info['meta']['code'] == 200:
         if len(user_info["data"]):
+           #printing details from user_info dictionary(Similar syntax for all JSON GET requests).
             print "Username: %s." % (user_info['data']['username'])
             print "Number of followers: %s." %(user_info['data']['counts']['followed_by'])
             print "Following: %s." % (user_info['data']['counts']['follows'])
@@ -73,6 +78,7 @@ def get_own_post():
 
     if own_media['meta']['code'] == 200:
         if (own_media['data']):
+            #Saving image name as image ID.
             image_name = own_media['data'][0]['id'] + '.jpeg'
             image_url = own_media ['data'][0]['images']['standard_resolution']['url']
             #code for downloading the selected image.
@@ -83,26 +89,62 @@ def get_own_post():
     else:
         print "Status code other than 200 received."
 
+def num_of_likes(insta_username):
+    user_id = get_user_id(insta_username)
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, ACCESS_TOKEN)
+    user_media = requests.get(request_url).json()
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            #Declaring dictionaries which will be copies of the dictionary of images with max and min likes respectively.
+            likes_count["min_id"] = 0
+            likes_count["max_id"] = 0
+            #Declaring dictionaries which will hold the value of number of likes.
+            likes_count['min'] = 0
+            likes_count['max'] = 0
+            #Condition for getting images with max and min number of likes.
+            for x in range(0, len(user_media['data'])):
+                if likes_count["min"] > user_media['data'][x]['likes']['count']:
+                    likes_count['min_id'] = x
+                    likes_count['min'] = user_media['data'][x]['likes']['count']
+                if likes_count['max'] < user_media['data'][x]['likes']['count']:
+                    likes_count['max_id'] = x
+                    likes_count['max'] = user_media['data'][x]['likes']['count']
+
 #Function for getting posts of a user.
 
 def get_user_post(insta_username):
+    # getting user id from username.
     user_id = get_user_id(insta_username)
-    if user_id == None:
-        print 'User does not exist!'
+    if user_id is None:
+        print "User doesn't exist"
         exit()
-    request_url = (BASE_URL + "users/%s/media/recent/?access_token=%s")%(user_id,ACCESS_TOKEN)
-    print ("GET request URL for getting posts of a user: %s") % (request_url)
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, ACCESS_TOKEN)
+    print 'Requestig url : %s' % (request_url)
     user_media = requests.get(request_url).json()
     if user_media['meta']['code'] == 200:
-        if len((user_media['data'])):
-            image_name = user_media['data']['0']['id'] + ".jpeg"
-            image_url = user_media['data'][0]['images']['standard_resolution']['url']
-            urllib.urlretrieve(image_url,image_name)
-            print "Your image has been downloaded."
+        if len(user_media['data']):
+            i = None
+            num_of_likes(insta_username)
+            print "Select criteria for selecting post : "
+            print "1) Minimum Likes.\n2) Maximum Likes."
+            post_choice = int(raw_input("Please Enter Your Post Selection Criteria : "))
+            while post_choice != 1 or 2 or 3:
+                if post_choice == 1:
+                    i = likes_count['min_id']
+                elif post_choice == 2:
+                    i = likes_count['max_id']
+                elif post_choice == 3:
+                    i = 0
+            # getting image information from user_media
+            image_name = user_media['data'][i]['id'] + ".jpeg"
+            img_url = user_media['data'][i]['images']['standard_resolution']['url']
+            # downloading the image using its url and saving it with img_name.
+            urllib.urlretrieve(img_url, image_name)
+            print "Your image has been downloaded!"
         else:
-            print"Post does not exist."
+            print "Post doesn't exist."
     else:
-        print "Status code other than 200 received."
+        print "Status code other than 200 received. [%d]" % user_media['meta']['code']
 
 #Function to get Media ID of the recent posts by a user.
 
@@ -147,7 +189,7 @@ def get_like_list(insta_username):
 def like_a_post(insta_username):
     media_id = get_post_id(insta_username)
     request_url = (BASE_URL + "media/%s/likes") % (media_id)
-    #payload dictionary for using POST.
+    #Declaring payload dictionary for using POST.
     payload = {"access_token": ACCESS_TOKEN}
     print "POST request URL : %s" % (request_url)
     #Code for posting a like.
@@ -202,7 +244,7 @@ def delete_negative_comment(insta_username):
             for x in range(0, len(comment_info['data'])):
                 comment_id = comment_info['data'][x]['id']
                 comment_text = comment_info['data'][x]['text']
-                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
+                blob = TextBlob(comment_text, analyzer = NaiveBayesAnalyzer())
                 #Condition for checking if the comment is positive or negative.
                 if (blob.sentiment.p_neg > blob.sentiment.p_pos):
                     print 'Negative comment : %s' % (comment_text)
@@ -221,6 +263,26 @@ def delete_negative_comment(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+#Defining tag names for an event through which user can search for an activity/event using its tag names.
+event_tags = ["tech","games"]
+
+#Function for searching by tagnames associated with an event(subtrends).
+
+def tag_search(insta_tagname):
+    request_url = (BASE_URL + "tags/search?q=%s&access_token=%s") % (insta_tagname,ACCESS_TOKEN)
+    print "GET request URL for Tag Search is : %s" % (request_url)
+    activity_subtrends = requests.get(request_url).json()
+    if activity_subtrends['meta']['code'] == 200:
+        if len(activity_subtrends['data']):
+            for x in range(0,len(activity_subtrends['data'])):
+                print "Name : " + activity_subtrends['data'][x]['name']
+                print ("Media count for tag is : %s") % (activity_subtrends['data'][x]['media_count'])
+        else:
+            print "Searched tag gave no results.!"
+    else:
+        print "Access code other than 200 received."
+
+
 #Function for showing menu of the bot from which user can select required function via standarad input.
 def start_bot():
     while True:
@@ -236,7 +298,8 @@ def start_bot():
         print "g.Get a list of comments on the recent post of a user.\n"
         print "h.Make a comment on the recent post of a user.\n"
         print "i.Delete negative comments from the recent post of a user.\n"
-        print "j.Exit."
+        print "j.Search for an event or activity using subtrends(tag).\n"
+        print "k.Exit."
 
         choice = raw_input("Enter you choice: ")
         #Various function calls encoded in the corresponding choices.
@@ -266,6 +329,15 @@ def start_bot():
            insta_username = raw_input("Enter the username of the user: ")
            delete_negative_comment(insta_username)
         elif choice == "j":
+            insta_tagname = raw_input("Enter the name of the tag without a leading # and without spaces. Eg.(nofilter,followorfollow)\n")
+            #Condition for searching the tag only if its an official tag of the event by comparing the input tag with the official tag list of the event.
+            for x in event_tags:
+                if x in insta_tagname:
+                    tag_search(insta_tagname)
+                    break
+                else:
+                    print "Invalid tag searched."
+        elif choice == "k":
             exit()
         else:
             print "Wrong choice. Select another option from the menu.!"
